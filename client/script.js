bf8784201a14d885c729e8b207287a5a22767576
Thomas Johnson <thomasij813@gmail.com>
2016-03-07 17:27:57 -0800

    Implement promises for making AJAX requests

--- client/script.js
+++ client/script.js
@@ -1,4 +1,27 @@
+function getData(url) {
+  return new Promise(function(resolve, reject) {
+    let request = new XMLHttpRequest();
+    request.open('GET', url, true);
 
+    request.onload = () => {
+      if (request.status >= 200 && request.status < 400) {
+        let response = request.responseText;
+        resolve(response);
+      } else {
+        reject('There was an error');
+      }
+    };
+
+    request.send();
+  });
+}
+
+getData('http://fcctop100.herokuapp.com/api/fccusers/top/recent')
+  .then((response) => {
+    console.log(response);
+  }, (error) => {
+    console.error(error);
+  });
 
 class App extends React.Component {
 
