a988c5e41a2ac30b7c8ac308890c3b43286f5910
Thomas Johnson <thomasij813@gmail.com>
2016-03-07 17:01:46 -0800

    Initial commit

---
+++ client/script.js
@@ -0,0 +1,14 @@
+
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="wrapper">
+        test
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("app"))
