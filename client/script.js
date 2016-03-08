545c904c04fef41b9c7c2cd713aeb424cee36f02
Thomas Johnson <thomasij813@gmail.com>
2016-03-07 17:41:07 -0800

    Make AJAX requests methods on App class and call within componentDidMount

--- client/script.js
+++ client/script.js
@@ -1,29 +1,42 @@
-function getData(url) {
-  return new Promise(function(resolve, reject) {
-    let request = new XMLHttpRequest();
-    request.open('GET', url, true);
-
-    request.onload = () => {
-      if (request.status >= 200 && request.status < 400) {
-        let response = request.responseText;
-        resolve(response);
-      } else {
-        reject('There was an error');
-      }
-    };
-
-    request.send();
-  });
-}
-
-getData('http://fcctop100.herokuapp.com/api/fccusers/top/recent')
-  .then((response) => {
-    console.log(response);
-  }, (error) => {
-    console.error(error);
-  });
-
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      topRecent: [],
+      topAllTime: []
+    };
+  }
+
+  getData(url) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      request.open('GET', url, true);
+
+      request.onload = () => {
+        if (request.status >= 200 && request.status < 400) {
+          let response = request.responseText;
+          resolve(response);
+        } else {
+          reject('There was an error');
+        }
+      };
+
+      request.send();
+    });
+  }
+
+  componentDidMount() {
+    // Get top recent
+    this.getData('http://fcctop100.herokuapp.com/api/fccusers/top/recent')
+      .then( (data) => { this.setState({ topRecent: data}); },
+             (error) => { console.error(error); });
+
+    // Get top all top app time
+    this.getData('http://fcctop100.herokuapp.com/api/fccusers/top/alltime')
+      .then( (data) => { this.setState({ topAllTime: data}); },
+             (error) => { console.error(error); });
+  }
 
   render() {
     return (
