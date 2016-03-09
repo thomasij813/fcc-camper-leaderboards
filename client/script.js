0d6657921c84ecc4294000f1fbcb9d5feda05881
Thomas Johnson <thomasij813@gmail.com>
2016-03-09 12:23:33 -0800

    Pull in bootstrap by cdn, add loading screen, improve toggling between datasets

--- client/script.js
+++ client/script.js
@@ -1,106 +1,135 @@
+const STORE = {
+  topRecent: {},
+  topAllTime: {}
+};
+
+function getData(url) {
+  return new Promise(function(resolve, reject) {
+    let request = new XMLHttpRequest();
+    request.open('GET', url, true);
+
+    request.onload = () => {
+      if (request.status >= 200 && request.status < 400) {
+        let response = request.responseText;
+        resolve(JSON.parse(response));
+      } else {
+        reject('There was an error');
+      }
+    };
+
+    request.send();
+  });
+}
+
 class App extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      table: 'topRecent',
-      ascending: true,
-      topRecent: [],
-      topAllTime: []
+      loading: true,
+      table: '',
+      data: [],
+      ascending: true
     };
   }
 
-  getData(url) {
-    return new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      request.open('GET', url, true);
-
-      request.onload = () => {
-        if (request.status >= 200 && request.status < 400) {
-          let response = request.responseText;
-          resolve(JSON.parse(response));
-        } else {
-          reject('There was an error');
-        }
-      };
-
-      request.send();
-    });
-  }
-
   componentDidMount() {
-    this.getData('http://fcctop100.herokuapp.com/api/fccusers/top/recent')
-      .then( (data) => { this.setState({ topRecent: data}); },
-             (error) => { console.error(error); });
-
-    this.getData('http://fcctop100.herokuapp.com/api/fccusers/top/alltime')
-      .then( (data) => { this.setState({ topAllTime: data}); },
-             (error) => { console.error(error); });
+    let dataRequests = [
+      getData('http://fcctop100.herokuapp.com/api/fccusers/top/recent'),
+      getData('http://fcctop100.herokuapp.com/api/fccusers/top/alltime')
+    ];
+    Promise.all(dataRequests).then(function(responses) {
+      STORE.topRecent = {
+        data: responses[0],
+        table: 'Recent'
+      };
+      STORE.topAllTime = {
+        data: responses[1],
+        table: 'All Time'
+      };
+      this.setState({
+        loading: false,
+        table: STORE.topRecent.table,
+        data: STORE.topRecent.data
+      });
+    }.bind(this));
   }
 
-  evaluateData() {
-    if (this.state.table === 'topRecent') {
-      if (this.state.ascending) {
-        return this.state.topRecent;
-      } else {
-        let reverse = this.state.topRecent.slice().reverse();
-        return reverse;
-      }
-
-    } else {
-      if (this.state.ascending) {
-        return this.state.topAllTime;
-      } else {
-        let reverse = this.state.topAllTime.slice().reverse();
-        return reverse;
-      }
-    }
-  }
-
-  evaluateSortArrow(table) {
-    if (this.state.table === table) {
-      if (this.state.ascending) {
-        return <span className="glyphicon glyphicon-triangle-top" onClick={this.toggleSort.bind(this)}></span>;
-      } else {
-        return <span className="glyphicon glyphicon-triangle-bottom" onClick={this.toggleSort.bind(this)}></span>;
-      }
-    }
-  }
-
-  toggleSort() {
-    let sort = this.state.ascending;
-    this.setState({ ascending: !sort });
-  }
-
-  setTable(e) {
-    let table = e.target.dataset.table;
+  onTableSelect(tableObj) {
     this.setState({
-      table: table,
-      ascending: true
+      table: tableObj.table,
+      data: tableObj.data
     });
   }
 
   render() {
-    return (
-      <div className="wrapper">
-          <table className="table table-hover table-sm">
-            <thead>
-              <tr >
-                <th>Camper</th>
-                <th className="text-center">
-                  <a href="#" onClick={this.setTable.bind(this)} data-table="topAllTime">Total Points</a>
-                  {this.evaluateSortArrow('topAllTime')}
-                </th>
-                <th className="text-center">
-                  <a href="#" onClick={this.setTable.bind(this)} data-table="topRecent">Recent Points (last 30 days)</a>
-                  {this.evaluateSortArrow('topRecent')}
-                </th>
-              </tr>
-            </thead>
-            <TableBody data={this.evaluateData()} />
-          </table>
+    if (this.state.loading) {
+      return <div>Loading</div>;
+    } else {
+      return (
+        <div className="container">
+          <h1 className="text-center">Free Code Camp Camper Leaderboards</h1>
+          <TableSelectorButtonGroup onTableSelect={this.onTableSelect.bind(this)} currentTable={this.state.table}/>
+            <table className="table table-hover table-sm">
+              <thead>
+                <tr >
+                  <th>Camper</th>
+                  <th className="text-center">
+                    <a href="#">Total Points</a>
+                  </th>
+                  <th className="text-center">
+                    <a href="#">Recent Points (last 30 days)</a>
+                  </th>
+                </tr>
+              </thead>
+              <TableBody data={this.state.data} />
+            </table>
+        </div>
+      )
+    }
+  }
+}
+
+class TableSelectorButtonGroup extends React.Component {
+  render() {
+    return(
+      <div className="btn-group btn-group-justified" role="group">
+        <TableSelectorButton
+          handleTableSelect={this.props.onTableSelect}
+          tableObj={STORE.topAllTime}
+          currentTable={this.props.currentTable}
+        />
+        <TableSelectorButton
+          handleTableSelect={this.props.onTableSelect}
+          tableObj={STORE.topRecent}
+          currentTable={this.props.currentTable}
+        />
       </div>
-    )
+    );
+  }
+}
+
+class TableSelectorButton extends React.Component {
+  handleClick() {
+    this.props.handleTableSelect(this.props.tableObj);
+  }
+  evaluateClassName() {
+    if (this.props.tableObj.table === this.props.currentTable) {
+      return 'btn btn-primary';
+    } else {
+      return 'btn btn-default';
+    }
+  }
+  render() {
+    return (
+      <a
+        type="button"
+        onClick={this.handleClick.bind(this)}
+        className={this.evaluateClassName()}
+      >
+         {this.props.tableObj.table}
+      </a>
+    );
   }
 }
 
