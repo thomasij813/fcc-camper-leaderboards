046ec045730d331e4410d652dfbb99d8dc4761cc
Thomas Johnson <thomasij813@gmail.com>
2016-03-09 16:12:38 -0800

    Add sort handler to column headers

--- client/script.js
+++ client/script.js
@@ -10,8 +10,13 @@ function getData(url) {
 
     request.onload = () => {
       if (request.status >= 200 && request.status < 400) {
-        let response = request.responseText;
-        resolve(JSON.parse(response));
+        let responseData = JSON.parse(request.responseText)
+          // Add rank to data
+          .map(function(user, index) {
+            user.rank = index + 1;
+            return user;
+          });
+        resolve(responseData);
       } else {
         reject('There was an error');
       }
@@ -62,6 +67,26 @@ class App extends React.Component {
     });
   }
 
+  onSort(sortValue) {
+    let sortedData;
+    if (sortValue === 'rank') {
+      sortedData = this.state.data.sort(function(a, b) {
+        return a[sortValue] - b[sortValue];
+      });
+    } else if (sortValue === 'username') {
+      sortedData = this.state.data.sort(function(a, b){
+          if(a[sortValue].toLowerCase() < b[sortValue].toLowerCase()) return -1;
+          if(a[sortValue].toLowerCase() > b[sortValue].toLowerCase()) return 1;
+          return 0;
+      });
+    } else {
+      sortedData = this.state.data.sort(function(a, b) {
+        return b[sortValue] - a[sortValue];
+      });
+    }
+    this.setState({ data: sortedData})
+  }
+
   render() {
     if (this.state.loading) {
       return <div>Loading</div>;
@@ -70,23 +95,33 @@ class App extends React.Component {
         <div className="container">
           <h1 className="text-center">Free Code Camp Camper Leaderboards</h1>
           <TableSelectorButtonGroup onTableSelect={this.onTableSelect.bind(this)} currentTable={this.state.table}/>
-            <table className="table table-hover table-sm">
-              <thead>
-                <tr >
-                  <th>Camper</th>
-                  <th className="text-center">
-                    <a href="#">Total Points</a>
-                  </th>
-                  <th className="text-center">
-                    <a href="#">Recent Points (last 30 days)</a>
-                  </th>
-                </tr>
-              </thead>
-              <TableBody data={this.state.data} />
-            </table>
+          <table className="table table-hover table-sm">
+            <thead>
+              <tr >
+                <ColumnHeader classNameProp="" headerTitle="Rank" handleSort={this.onSort.bind(this)} sortValue="rank"/>
+                <ColumnHeader classNameProp="" headerTitle="Camper" handleSort={this.onSort.bind(this)} sortValue="username"/>
+                <ColumnHeader classNameProp="text-center" headerTitle="Total Points" handleSort={this.onSort.bind(this)} sortValue="alltime"/>
+                <ColumnHeader classNameProp="text-center" headerTitle="Recent Points (last 30 days)" handleSort={this.onSort.bind(this)} sortValue="recent"/>
+              </tr>
+            </thead>
+            <TableBody data={this.state.data} />
+          </table>
         </div>
       )
     }
+  }
+}
+
+class ColumnHeader extends React.Component {
+  handleSort() {
+    this.props.handleSort(this.props.sortValue);
+  }
+  render() {
+    return(
+      <th className={this.props.classNameProp}>
+        <a href="#" onClick={this.handleSort.bind(this)}>{this.props.headerTitle}</a>
+      </th>
+    );
   }
 }
 
@@ -140,6 +175,7 @@ class TableBody extends React.Component {
         {this.props.data.map( (item, index) => {
           return (
             <tr key={index}>
+              <td>{item.rank}</td>
               <td>
                 <img src={item.img} className="user_image"/> <a href={`http://www.freecodecamp.com/${item.username}`}>{item.username}</a>
                 </td>
